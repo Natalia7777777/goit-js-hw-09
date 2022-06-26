2d754eac243de28b46c2dcf13febaaa882dd79b6
Natalia Bubnova <bubnova.natalja89@gmail.com>
2022-06-26 20:12:55 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const bodyColor = document.querySelector('body');
+const colorBtnStart = document.querySelector('button[data-start]');
+const colorBtnStop = document.querySelector('button[data-stop]');
 
+colorBtnStart.addEventListener("click", start);
+colorBtnStop.addEventListener("click", stop);
+
+let intervalId = null;
+colorBtnStart.disabled = false;
+
+function start() {
+    intervalId = setInterval(() => {
+        const color = getRandomHexColor();
+        bodyColor.style.backgroundColor = color;
+    }, 1000);
+    colorBtnStart.disabled = true;
+}
+
+function stop() {
+    clearInterval(intervalId);
+    colorBtnStart.disabled = false;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
